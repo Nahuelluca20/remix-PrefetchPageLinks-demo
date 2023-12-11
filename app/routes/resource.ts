1ec7c7da3e381f8af79a6daf0eb1b74cd6875627
Nahuel Luca <nahuelluca20@gmail.com>
2023-12-11 13:39:25 -0300

    Update message in resource.ts

--- app/routes/resource.ts
+++ app/routes/resource.ts
@@ -3,9 +3,9 @@ import { json } from "@remix-run/node";
 
 export const loader: LoaderFunction = async () => {
   await new Promise((resolve) => setTimeout(resolve, 5000));
-  const data = { message: "Data" };
-  let headers = { "Cache-Control": "public, max-age=60" };
+  const data = { message: "This is a message" };
+  // let headers = { "Cache-Control": "public, max-age=60" };
   return json(data, {
-    headers,
+    // headers,
   });
 };
