bf5b401954be9643c68d440323313debc469111f
Nahuel Luca <nahuelluca20@gmail.com>
2023-12-11 12:21:57 -0300

    Add PrefetchPageLinks and useFetcher to improve data loading

---
+++ app/routes/resource.ts
@@ -0,0 +1,11 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+
+export const loader: LoaderFunction = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const data = { message: "Data" };
+  let headers = { "Cache-Control": "public, max-age=60" };
+  return json(data, {
+    headers,
+  });
+};
